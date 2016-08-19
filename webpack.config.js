48b19a3e4f66657f2f0d1c0948c1975c5efa8bb4
Christian Alfoni <christianalfoni@gmail.com>
2016-08-19 12:14:33 +0200

    Fixed issue where options are not defined on listeners

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var loaders = [
+const loaders = [
   {
-    "test": /\.js?$/,
-    "exclude": /node_modules/,
-    "loader": "babel",
-    "query": {
-      "presets": [
-        "es2015",
-        "stage-0"
+    'test': /\.js?$/,
+    'exclude': /node_modules/,
+    'loader': 'babel',
+    'query': {
+      'presets': [
+        'es2015',
+        'stage-0'
       ],
-      "plugins": []
+      'plugins': []
     }
   }
 ];
